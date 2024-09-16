aa112609482bfdc7641e449ae5f9518bc02b764e
Outlier <nzombeebenezer@gmail.com>
2024-09-16 19:17:40 +0100

    JWT section

--- 05-JWT-Basics/starter/db/connect.js
+++ 05-JWT-Basics/starter/db/connect.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  return mongoose.connect(url)
 }
 
 module.exports = connectDB
